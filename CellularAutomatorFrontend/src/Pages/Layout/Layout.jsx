409237b6dfee19bfedc5879f880198b626f4fe9c
Atromat <atromatok@gmail.com>
2024-05-17 00:10:06 +0200

    Add styling to the navigation bar

--- CellularAutomatorFrontend/src/Pages/Layout/Layout.jsx
+++ CellularAutomatorFrontend/src/Pages/Layout/Layout.jsx
@@ -1,25 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, Outlet } from 'react-router-dom';
 import './Layout.css';
 
 function Layout() {
+  const [activePage, setActivePage] = useState('home');
+  const [responsiveToggled, setResponsiveToggled] = useState(false);
+
   return (
     <div className="Layout">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/RuleEditor">Rule Editor</Link>
-          </li>
-          <li>
-          <Link to="/Login">Login</Link>
-          </li>
-          <li>
-          <Link to="/Register">Register</Link>
-          </li>
-        </ul>
+      <nav className={ responsiveToggled ? 'Topnav Responsive' : 'Topnav' }>
+        <Link to="/" className='NavbarLink' setActivePage={setActivePage}>Home</Link>
+        <Link to="/RuleEditor" className='NavbarLink' setActivePage={setActivePage}>Rule Editor</Link>
+        <Link to="/SignIn" className='NavbarLink' setActivePage={setActivePage}>Sign In</Link>
+        <Link to="/Register" className='NavbarLink' setActivePage={setActivePage}>Register</Link>
+        <a href="javascript:void(0);" className="icon NavbarLink" onClick={(e) => setResponsiveToggled(!responsiveToggled)}>
+          <i className="fa fa-bars"></i>
+        </a>
       </nav>
       <Outlet />
   </div>
