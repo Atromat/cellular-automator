dc6e4f9bdc29af915e8e13de864ab27738928b7c
Atromat <atromatok@gmail.com>
2024-06-10 01:29:38 +0200

    Add index.js to configure express and mongodb connection

---
+++ CellularAutomatorBackend/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+
+const { MONGO_URL, PORT = 8080 } = process.env;
+
+if (!MONGO_URL) {
+  console.error("Missing MONGO_URL environment variable");
+  process.exit(1);
+}
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(MONGO_URL, { useNewUrlParser: true, 
+                                    useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.error(err));
+
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
