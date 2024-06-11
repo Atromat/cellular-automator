ad8a2c40b7409bcb4422561587d80e334df2861f
Atromat <atromatok@gmail.com>
2024-06-12 01:43:16 +0200

    Add ruleset schema and rulesets property to UserSchema

--- CellularAutomatorBackend/models/User.js
+++ CellularAutomatorBackend/models/User.js
@@ -1,5 +1,75 @@
 const mongoose = require('mongoose');
 
+const coordRelativeToCell = new mongoose.Schema({
+    _id: false,
+    r: {
+        type: Number, 
+        required: true
+    },
+    c: {
+        type: Number, 
+        required: true
+    },
+})
+
+const pattern = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: true
+    },
+    coordsRelativeToCell: [coordRelativeToCell],
+    cellTypeToCheck: {
+        type: Number, 
+        required: true
+    },
+    operation: {
+        type: String,
+        required: true
+    },
+    min: Number,
+    max: Number
+})
+
+const rule = new mongoose.Schema({
+    ruleName: {
+        type: String, 
+        required: true
+    },
+    effectsCellType: {
+        type: Number, 
+        required: true
+    },
+    patterns: [],
+    cellBecomes: {
+        type: Number, 
+        required: true
+    },
+})
+
+const cellType = new mongoose.Schema({
+    _id: false,
+    id: {
+        type: Number, 
+        required: true
+    },
+    cellType: {
+        type: String,
+        required: true
+    },
+    cellColor: {
+        type: String,
+        required: true
+    }
+})
+
+const ruleset = new mongoose.Schema({
+    ruleSetName: {
+        type: String, 
+        required: true
+    },
+    cellTypes: [cellType]
+})
+
 const UserSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -9,7 +79,8 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    rulesets: []
 });
 
 module.exports = mongoose.model('User', UserSchema);
