9d1ac3e12e34749783f13b2110611902496362d2
Atromat <atromatok@gmail.com>
2024-06-12 14:52:48 +0200

    Change App component behaviour to react properly localStorage and movel handleLogout to Layout

--- CellularAutomatorFrontend/src/Pages/Layout/Layout.jsx
+++ CellularAutomatorFrontend/src/Pages/Layout/Layout.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react'
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import { Link, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import './Layout.css';
 
-function Layout({signedInUser, handleLogout}) {
+function Layout({isUserSignedIn, setIsUserSignedIn}) {
   const [responsiveToggled, setResponsiveToggled] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogout = (event) => {
+    localStorage.removeItem('userToken');
+    setIsUserSignedIn(false);
+    navigate('/');
+  };
 
   function addActivePageToClassNameIfLocationEqual(location, originalClassName) {
     return useLocation().pathname === location ? originalClassName + ' ActivePage' : originalClassName;
@@ -13,15 +20,19 @@ function Layout({signedInUser, handleLogout}) {
     <div className="Layout">
       <nav className={ responsiveToggled ? 'Topnav Responsive' : 'Topnav' }>
         <Link to="/" className={addActivePageToClassNameIfLocationEqual('/', 'NavbarLink')}>Cellular Automator</Link>
-        <Link to="/RuleEditor" className={addActivePageToClassNameIfLocationEqual('/RuleEditor', 'NavbarLink')}>Rule Editor</Link>
-        {!signedInUser ? (
+        
+        {!isUserSignedIn ? (
           <>
           <Link to="/SignIn" className={addActivePageToClassNameIfLocationEqual('/SignIn', 'NavbarLink')}>Sign In</Link>
           <Link to="/Register" className={addActivePageToClassNameIfLocationEqual('/Register', 'NavbarLink')}>Register</Link>
           </>
         ) : (
+          <>
+          <Link to="/RuleEditor" className={addActivePageToClassNameIfLocationEqual('/RuleEditor', 'NavbarLink')}>Rule Editor</Link>
           <div className='SignOutDiv NavbarLink' onClick={(e) => handleLogout(e)} >Sign Out</div>
+          </>
         )}
+
         <div className="icon NavbarLink" onClick={(e) => setResponsiveToggled(!responsiveToggled)}>
           <i className="fa fa-bars"></i>
         </div>
