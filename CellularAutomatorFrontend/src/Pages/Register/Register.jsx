4a461824bf0db01b08ca35ce6c28f3393f84255b
Atromat <atromatok@gmail.com>
2024-06-12 14:43:46 +0200

    Remove setSignedInUser from Register component

--- CellularAutomatorFrontend/src/Pages/Register/Register.jsx
+++ CellularAutomatorFrontend/src/Pages/Register/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-function Register({apiURL, setSignedInUser}) {
+function Register({apiURL}) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
