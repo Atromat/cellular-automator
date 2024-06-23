3116ae9570f4ef9141e90a902947c6a3ace50023
Atromat <atromatok@gmail.com>
2024-06-23 20:28:13 +0200

    Add server mock for post /auth/signin endpoint

--- CellularAutomatorFrontend/tests/setup.js
+++ CellularAutomatorFrontend/tests/setup.js
@@ -4,6 +4,26 @@ import * as matchers from "@testing-library/jest-dom/matchers";
 
 expect.extend(matchers);
 
+import { setupServer } from 'msw/node';
+import { HttpResponse, http } from 'msw';
+
+import { API_URL } from '../src/api';
+
+export const restHandlers = [
+  http.post(`${API_URL}/auth/signin`, (request) => {
+    const token = "tokenString";
+    const res = {token: token};
+    return HttpResponse.json(res);
+  }),
+]
+
+const server = setupServer(...restHandlers);
+
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }));
+
+afterAll(() => server.close());
+
 afterEach(() => {
+  server.resetHandlers();
   cleanup();
 });
