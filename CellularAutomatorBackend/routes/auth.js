8aac727061d8a32ad74cdbfc60d6c8fa3269deff
Atromat <atromatok@gmail.com>
2024-07-21 22:32:22 +0200

    Remove 2 unnecessary console.log-s

--- CellularAutomatorBackend/routes/auth.js
+++ CellularAutomatorBackend/routes/auth.js
@@ -14,6 +14,8 @@ router.post('/register', async (req, res) => {
 
     try {
         let user = await User.findOne({ email });
+        console.log("user:")
+        console.log(user)
         if (user) {
             return res.status(400).json({ msg: 'User already exists' });
         }
